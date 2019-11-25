8171f61082dc3bae8e0a491da1611a89a79c70ef
paczulapiotr <paczulapiotr@gmail.com>
2019-11-25 17:50:26 +0100

    Remove unused scripts

--- Scripts/addmigration.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd %0\..\..
-dotnet restore
-dotnet ef migrations add %1 -s .\Mimir.API\Mimir.API.csproj -p .\Mimir.Database\Mimir.Database.csproj -c MimirDbContext
-popd
-set /p DUMMY=Hit ENTER to continue...
